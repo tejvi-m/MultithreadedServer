9d62cfbf527cefca92148463c18b72f5ecc6c7ff
Tejvi M <tejvimstays@gmail.com>
2019-11-09 15:59:42 +0530

    working interface, can recieve and send the required info.

--- Cserver/Interface.py
+++ Cserver/Interface.py
@@ -1,9 +1,34 @@
 import socket
 import time
+import tkinter as tk
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 9999))
+client.connect(('localhost', 5008))
+#
+# time.sleep(0.2)
+# print ('Received message:', client.recv(1024))
 
-print (client.send('Hello world!'), ' bytes sent.')
-time.sleep(0.2)
-print ('Received message:', client.recv(1024))
+
+def show_entry_fields():
+    client.send(e1.get().encode())
+    print("File requested. will be copied to the current directory under the same name.")
+    T = tk.Text(master, height=2, width=30)
+    T.grid(row=5, column = 0)
+    T.insert(tk.END, client.recv(1024).decode())
+    # print ('Received message:', )
+
+master = tk.Tk()
+tk.Label(master,
+         text="First Name").grid(row=0)
+
+e1 = tk.Entry(master)
+
+e1.grid(row=0, column=1)
+
+tk.Button(master,
+          text='Request', command=show_entry_fields).grid(row=3,
+                                                       column=1,
+                                                       sticky=tk.W,
+                                                       pady=4)
+
+tk.mainloop()
