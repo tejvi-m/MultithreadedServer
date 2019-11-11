d469fe9c6bebb622eef8a8bab13034cb9003756a
Tejvi M <tejvimstays@gmail.com>
2019-11-11 20:12:29 +0530

    minor changes wrt filenames

--- Cserver/server.c
+++ Cserver/server.c
@@ -9,7 +9,7 @@
 
 #include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h> 	
+#include <fcntl.h>
 #include <dirent.h>
 #include <pthread.h>
 
@@ -25,7 +25,7 @@ void* clientAction(void* arg){
 	char data_to_send[1024];
 	char client_message[2048];
 	int client_fd = *((int *) arg);
-	
+
 	recv(client_fd, client_message , 2048, 0);
 	//client_message, which is a request, should be processed accoding to application, here maybe for a file.//To be done later.
 	int bytes_read;
@@ -38,7 +38,7 @@ void* clientAction(void* arg){
 	// /home/thejas/MultithreadedServer/a.txt
 	int fd = open(path, O_RDONLY);
 	if (fd!=-1){
-		
+
 		while((bytes_read=read(fd, data_to_send, 1024))>0)
 			send(client_fd, data_to_send, bytes_read, 0);
 
@@ -57,15 +57,15 @@ int main()
 {
 	int sockfd = 0;				        // listen - listen file descriptor
 	int connfd = 0;					// connect - connect file descriptor
-	char recvBuff[256];				
-	struct sockaddr_in serv_addr;			// A sockaddr_in is a structure containing an internet address. 
+	char recvBuff[256];
+	struct sockaddr_in serv_addr;			// A sockaddr_in is a structure containing an internet address.
 							// This structure is defined in <netinet/in.h>.
 	char sendBuff[1024];
 	int numrv;
 	char filename[1024];
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);	// listen - listen for connections on a socket
-							// AF_INET refers to the address family ipv4. 
+							// AF_INET refers to the address family ipv4.
 							// The SOCK_STREAM means connection oriented TCP protocol.
 
 	printf("Socket retrieve success\n");
@@ -73,11 +73,11 @@ int main()
   	memset(&serv_addr, '0', sizeof(serv_addr));	// memset() is used to fill a block of memory with a particular value.
 	memset(sendBuff, '0', sizeof(sendBuff));
 
-	serv_addr.sin_family = AF_INET;			// A sockaddr_in is a structure containing an internet address. 
+	serv_addr.sin_family = AF_INET;			// A sockaddr_in is a structure containing an internet address.
 							// This structure is defined in <netinet/in.h>.
 
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);	
-	serv_addr.sin_port = htons(5054);		// port:5050
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(5056);		// port:5050
 
 	bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));	//        bind - bind a name to a socket
 
@@ -91,9 +91,14 @@ int main()
 	pthread_t tid[60]; //handle 60 clients as of now.
 	int client_accepted;
 	int i = 0;
-	while (1)
+  ROOT = getenv("PWD");
+
+  int lenr = strlen(ROOT);
+  ROOT[lenr] = '/';
+  ROOT[lenr + 1] = '\0';
+
+  while (1)
   	{
-		ROOT = getenv("PWD");
 		client_accepted = accept(sockfd, (struct sockaddr *)NULL, NULL);
 		if(client_accepted < 0){
 			printf("error");
@@ -101,17 +106,17 @@ int main()
 		}
 		else{
 			//pthread_create(&tid[i], NULL, socketThread, &newSocket)
-            if (pthread_mutex_init(&lock, NULL) != 0) { 
-                printf("mutex init failed\n"); 
-                return 1; 
-            } 
+            if (pthread_mutex_init(&lock, NULL) != 0) {
+                printf("mutex init failed\n");
+                return 1;
+            }
 			if(pthread_create(&tid[i], NULL, clientAction, &client_accepted) != 0){
 				printf("failed");
 
 			}
 			else{
-				pthread_join(tid[i++],NULL);			
-			
+				pthread_join(tid[i++],NULL);
+
 			}
 		}
 		if(i >= 60){
@@ -123,11 +128,10 @@ int main()
 
 		// If client sends a GET
 
-		
+
   	}
-    
+
     pthread_mutex_destroy(&lock);
 	return 0;
-	
-}
 
+}
